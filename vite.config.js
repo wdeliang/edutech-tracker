d173be1dd936e5f0167714873c6d77ada545e1f1
wdeliang <wdeliang@connect.hku.hk>
2026-05-24 19:14:40 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [vue()],
+  base: '/edutech-tracker/'
+})
